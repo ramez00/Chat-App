5a7773dc1837504777b6a888fdee86bd9cf7d005
ramez.abdelhamid@health-insights.com <ramez.abdelhamid@health-insights.com>
2025-05-17 14:56:23 +0300

    Add project files.

---
+++ Chat-App/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chat_App
+{
+    [HubName("chat")]
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string name, string message)
+        {
+            
+            Clients.All.newMessage(name, message);
+        }
+    }
+}
---
+++ Chat-App/Startup1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(Chat_App.Startup1))]
+
+namespace Chat_App
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
