dd8241279261863991b9401b1b3f8907582b2721
ramez.abdelhamid@health-insights.com <ramez.abdelhamid@health-insights.com>
2025-05-18 19:49:58 +0300

    - Implement Option To Add Group name and Send Message to all group members - Send Message to All Group members Once any Member Joined to group

--- Chat-App/ChatHub.cs
+++ Chat-App/ChatHub.cs
@@ -15,5 +15,18 @@ namespace Chat_App
             
             Clients.All.newMessage(name, message);
         }
+
+        public void SendGroup(string name, string message,string grpName)
+        {
+
+            Clients.Group(grpName).messageGrp(message,name,grpName);
+        }
+
+        public void joinGroup(string name, string grpName)
+        {
+            Groups.Add(Context.ConnectionId, grpName);
+
+            Clients.OthersInGroup(grpName).newMember(name, grpName);
+        }
     }
 }
